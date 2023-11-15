63d00b1605017c7be7a74a511c3321d72d973a06
Peter Johan Arkadhira Setiabudi <peterjas03@gmail.com>
2023-11-15 22:23:35 +0700

    Update test_pwPlain.py

--- test_pwPlain.py
+++ test_pwPlain.py
@@ -4,34 +4,34 @@ from PythonPlain import validPassword1
 
 class testPassword(unittest.TestCase):
     def testValidPW(self):
-        self.assertTrue(validPassword1("Peterjas04!"))
-        self.assertTrue(validPassword1("PJAS04!@"))
-        self.assertTrue(validPassword1("PETERJOHAN@")) 
+        self.assertTrue(validPassword1("Haikalmzg03@"))
+        self.assertTrue(validPassword1("HAIKALMZG@"))
+        self.assertTrue(validPassword1("Kall4!")) 
     
     def testLengthPW(self):
-        self.assertFalse(validPassword1("Pijo4!")) # Length less than 8
-        self.assertFalse(validPassword1("Piiiiiiiijoooo0!")) # Length more than 15
+        self.assertFalse(validPassword1("Kall4!")) # Length less than 8
+        self.assertFalse(validPassword1("Haikalllllllllll0!")) # Length more than 15
         
     def testInvalidCharacterPW(self):
-        self.assertFalse(validPassword1("peterjohan04@")) # No Capital Letter
-        self.assertFalse(validPassword1("Peterjohan04")) # No Special Character
-        self.assertFalse(validPassword1("peterjohan04")) # No capital letters or special characters
-        self.assertFalse(validPassword1("12345434")) # only numbers
-        self.assertFalse(validPassword1("47563743!")) # no alphabetical characters
-        self.assertFalse(validPassword1("PeterJohan04^")) # Unaccepted Special Character
+        self.assertFalse(validPassword1("haikalmsg03@")) # No Capital Letter
+        self.assertFalse(validPassword1("hAikalmzg03")) # No Special Character
+        self.assertFalse(validPassword1("haikalmzg03")) # No capital letters or special characters
+        self.assertFalse(validPassword1("12345678")) # only numbers
+        self.assertFalse(validPassword1("haikalmzg")) # only lowercase letters
+        self.assertFalse(validPassword1("HaikalMZG04^")) # Unaccepted Special Character
         
     def testSpecialPW(self):
-        self.assertFalse(validPassword1("!Pijo0408")) # Special character in front
-        self.assertFalse(validPassword1("$Pijo0408")) # Special character in front
-        self.assertFalse(validPassword1("*Pijo0408")) # Special character in front
-        self.assertFalse(validPassword1("&Pijo0408")) # Special character in front
-        self.assertFalse(validPassword1("@Pijo0408")) # Special character in front
-        self.assertFalse(validPassword1("#Pijo0408")) # Special character in front
-        self.assertFalse(validPassword1("+Pijo0408")) # Special character in front
-        self.assertFalse(validPassword1("=Pijo0408")) # Special character in front
-        self.assertFalse(validPassword1(")Pijo0408")) # Special character in front
-        self.assertFalse(validPassword1("/Pijo0408")) # Special character in front
-        self.assertFalse(validPassword1("(Pijo0408")) # Special character in front
+        self.assertFalse(validPassword1("!HaikalMzg03")) # Special character in front
+        self.assertFalse(validPassword1("$HaikalMzg03")) # Special character in front
+        self.assertFalse(validPassword1("*HaikalMzg03")) # Special character in front
+        self.assertFalse(validPassword1("&HaikalMzg03")) # Special character in front
+        self.assertFalse(validPassword1("@HaikalMzg03")) # Special character in front
+        self.assertFalse(validPassword1("#HaikalMzg03")) # Special character in front
+        self.assertFalse(validPassword1("+HaikalMzg03")) # Special character in front
+        self.assertFalse(validPassword1("=HaikalMzg03")) # Special character in front
+        self.assertFalse(validPassword1(")HaikalMzg03")) # Special character in front
+        self.assertFalse(validPassword1("/HaikalMzg03")) # Special character in front
+        self.assertFalse(validPassword1("(HaikalMzg03")) # Special character in front
 
 if __name__ == '__main__':
     unittest.main()
